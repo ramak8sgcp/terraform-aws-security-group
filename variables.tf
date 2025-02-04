4426bbbd929d9735b94ac7a3222eba05e3436b3d
ramak8sgcp <ramak8sgcp@gmail.com>
2025-02-04 18:36:53 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+  
+}
+
+variable "environment" {
+  
+}
+
+variable "sg_name" {
+  
+}
+
+variable "common_tags" {
+  
+}
+
+variable "sg_tags" {
+  
+}
+
+variable "vpc_id" {
+  
+}
